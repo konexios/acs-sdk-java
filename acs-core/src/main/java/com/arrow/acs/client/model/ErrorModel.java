1f538cf1d4e23ca1c19869361c4b695923670cf2
Tam Nguyen <tnguyen@arrow.com>
2017-04-07 16:53:22 -0500

    added ErrorModel

---
+++ acs-core/src/main/java/com/arrow/acs/client/model/ErrorModel.java
@@ -0,0 +1,67 @@
+package com.arrow.acs.client.model;
+
+import java.io.Serializable;
+
+public class ErrorModel implements Serializable {
+	private static final long serialVersionUID = 5473020814782111757L;
+
+	private String code;
+	private String message;
+
+	public ErrorModel withCode(String code) {
+		setCode(code);
+		return this;
+	}
+
+	public ErrorModel withMessage(String message) {
+		setMessage(message);
+		return this;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((code == null) ? 0 : code.hashCode());
+		result = prime * result + ((message == null) ? 0 : message.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ErrorModel other = (ErrorModel) obj;
+		if (code == null) {
+			if (other.code != null)
+				return false;
+		} else if (!code.equals(other.code))
+			return false;
+		if (message == null) {
+			if (other.message != null)
+				return false;
+		} else if (!message.equals(other.message))
+			return false;
+		return true;
+	}
+}
