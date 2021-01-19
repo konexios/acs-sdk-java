dc8e86e119db8dc788bf2730069b0a4ce0914af1
Tam Nguyen <tam@konexios.com>
2021-01-19 08:40:49 -0600

    added connection monitor and cleanup for Apache HTTP client

--- acs-core/src/main/java/com/arrow/acs/client/api/ConnectionManager.java
+++ acs-core/src/main/java/com/arrow/acs/client/api/ConnectionManager.java
@@ -10,6 +10,10 @@
  *******************************************************************************/
 package com.arrow.acs.client.api;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
 import javax.annotation.PreDestroy;
 
 import org.apache.http.config.SocketConfig;
@@ -27,6 +31,10 @@ public class ConnectionManager extends Loggable {
 	private static final int DEFAULT_VALIDATE_AFTER_INACTIVITY_MS = 5000;
 	private static final int DEFAULT_SOCKET_TIMEOUT_MS = 600000;
 
+	private static final long DEFAULT_CLOSE_IDLE_CONNECTION_SECS = 30L;
+	private static final long DEFAULT_CONNECTION_MONITOR_DELAY_SECS = 30L;
+	private static final long DEFAULT_CONNECTION_MONITOR_INTERVAL_SECS = 5L;
+
 	private static final class SingletonHolder {
 		static final ConnectionManager SINGLETON = new ConnectionManager();
 	}
@@ -41,6 +49,8 @@ public class ConnectionManager extends Loggable {
 	private int maxTotalConnections = DEFAULT_MAX_TOTAL_CONNECTIONS;
 	private int maxPerRouteConnections = DEFAULT_MAX_PER_ROUTE_CONNECTIONS;
 
+	private ScheduledFuture<?> connectionMonitor;
+
 	private ConnectionManager() {
 		String method = "ConnectionManager";
 		logInfo(method, "maxTotalConnections: %d", maxPerRouteConnections);
@@ -50,6 +60,18 @@ public class ConnectionManager extends Loggable {
 		connectionManager.setValidateAfterInactivity(DEFAULT_VALIDATE_AFTER_INACTIVITY_MS);
 		connectionManager.setDefaultSocketConfig(
 				SocketConfig.custom().setSoKeepAlive(true).setSoTimeout(DEFAULT_SOCKET_TIMEOUT_MS).build());
+
+		logInfo(method, "scheduling connection monitor thread ...");
+		connectionMonitor = Executors.newSingleThreadScheduledExecutor().scheduleWithFixedDelay(() -> {
+			try {
+				connectionManager.closeExpiredConnections();
+			} catch (Exception e) {
+			}
+			try {
+				connectionManager.closeIdleConnections(DEFAULT_CLOSE_IDLE_CONNECTION_SECS, TimeUnit.SECONDS);
+			} catch (Exception e) {
+			}
+		}, DEFAULT_CONNECTION_MONITOR_DELAY_SECS, DEFAULT_CONNECTION_MONITOR_INTERVAL_SECS, TimeUnit.SECONDS);
 
 		// initialize shared client
 		sharedClient = HttpClients.custom().setConnectionManager(connectionManager).setConnectionManagerShared(true)
@@ -82,6 +104,13 @@ public class ConnectionManager extends Loggable {
 	@PreDestroy
 	public void shutdown() {
 		String method = "shutdown";
+		if (connectionMonitor != null) {
+			try {
+				connectionMonitor.cancel(true);
+			} catch (Throwable t) {
+			}
+			connectionMonitor = null;
+		}
 		if (connectionManager != null) {
 			try {
 				sharedClient = null;
